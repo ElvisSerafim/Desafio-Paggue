56872b70819bf32a959b3fab8a77b65349f27069
ElvisSerafim <elvisdouza@outlook.com>
2022-07-08 01:35:52 -0300

    Changing Header according to access.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState, useContext } from 'react';
 import * as C from '../../styles/styles';
 import Profile from '../../assets/profile.png';
 import Logo from '../../assets/logo.png';
 import {FaBars} from 'react-icons/fa';
 import Sidebar from '../SideBar';
 
-export default function Header(props){
+import AuthContext from '../../contexts/auth';
 
+
+export default function Header(props){
+    const { signed } = useContext(AuthContext);
     const [showSideBar, setShowSideBar] = useState(false);
 
     const openSideBar = () => setShowSideBar(!showSideBar);
 
-    useEffect(() => {
-
-        console.log(showSideBar);
-
-    }, [showSideBar])
-
     return (
         <React.Fragment>
             <C.ContainerHeader>
-                {props.logged === true ? (
+                {signed === true ? (
                     <FaBars size={25} cursor={'pointer'} onClick={openSideBar} />
                 ): <></>}
                 {showSideBar && <Sidebar active={openSideBar} />} 
