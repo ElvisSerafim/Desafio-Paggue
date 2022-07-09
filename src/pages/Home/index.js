4fa4406500a0be245101611e1955dbe88e90c60c
ElvisSerafim <elvisdouza@outlook.com>
2022-07-08 22:56:16 -0300

    Create the Home page.

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as C from '../../styles/styles';
+
+
+export default function Home() {
+
+
+    return (
+        <React.Fragment>
+            <C.ContainerColumn>
+                <C.Title>SEJA BEM VINDO AO ECOMMERCE PAGGUE !</C.Title>
+                <C.Subtitle>Navegue entre as opções no menu lateral para gerenciar sua loja.</C.Subtitle>
+            </C.ContainerColumn>
+        </React.Fragment>
+    );
+}
