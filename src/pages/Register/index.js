fb8434d02f40fba3bc0eb0b002f4fe5b1f55f20f
ElvisSerafim <elvisdouza@outlook.com>
2022-07-08 22:58:18 -0300

    Add function to create User and redirect to Home.

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,103 +1,140 @@
-import React, {  } from 'react';
+import React, {useState, useContext} from 'react';
 import {
     Input,
     Stack,
     Button
 } from '@chakra-ui/react';
 import * as C from '../../styles/styles';
+import { api } from '../../services/api';
+import AuthContext from '../../contexts/auth';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Register() {
 
+    let navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phone, setPhone] = useState('');
+    const [razaoSocial, setRazaoSocial] = useState('');
+    const [nomeFantasia, setNomeFantasia] = useState('');
+    const [cpfCnpj, setCpfCnpj] = useState('');
+    const [name, setName] = useState('');
+
+    const context = useContext(AuthContext);
+
+
+    const createUser = () => {
+        
+        const user = {
+            email: email,
+            password: password,
+            cpf_cnpj: cpfCnpj,
+            name: name,
+            phone: phone,
+            razao_social: razaoSocial,
+            nome_fantasia: nomeFantasia,
+            id: Math.floor(Math.random * 100000000)
+        }
+        api.post('users/', user);
+        context.setCurrentUser(user);
+        navigate('/profile');
+    }
+
+
     return (
         <React.Fragment>
-                <C.ContainerColumn>
-                    <C.Title>Cadastro</C.Title>
-                    <C.ContainerRow>
-                        <C.ContainerRegisterFields>
-                            <Stack spacing={5}>
-                                <C.DivInput>
-                                    <C.TextInput>Nome Completo</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='email'
-                                        placeholder='Endereço de Email'
-                                    />
-                                </C.DivInput>
+            <C.ContainerColumn>
+                <C.Title>Cadastro</C.Title>
+                <C.ContainerRow>
+                    <C.ContainerRegisterFields>
+                        <Stack spacing={5}>
+                            <C.DivInput>
+                                <C.TextInput>Nome Completo</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    name='email'
+                                    value={name}
+                                    onChange={e => setName(e.target.value)}
+                                />
+                            </C.DivInput>
 
-                                <C.DivInput>
-                                    <C.TextInput>Email</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='password'
-                                        placeholder='Senha'
-                                        type={'password'}
-                                    />
-                                </C.DivInput>
-                                <C.DivInput>
-                                    <C.TextInput>Razão Social</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='password'
-                                        placeholder='Senha'
-                                        type={'password'}
-                                    />
-                                </C.DivInput>
-                                <C.DivInput>
-                                    <C.TextInput>Senha</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='password'
-                                        placeholder='Senha'
-                                        type={'password'}
-                                    />
-                                </C.DivInput>
+                            <C.DivInput>
+                                <C.TextInput>Email</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    type={'email'}
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </C.DivInput>
+                            <C.DivInput>
+                                <C.TextInput>Razão Social</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    value={razaoSocial}
+                                    onChange={e => setRazaoSocial(e.target.value)}
+                                />
+                            </C.DivInput>
+                            <C.DivInput>
+                                <C.TextInput>Senha</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    name='password'
+                                    placeholder='Senha'
+                                    type={'password'}
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                            </C.DivInput>
 
-                            </Stack>
+                        </Stack>
 
-                        </C.ContainerRegisterFields>
-                        <C.ContainerRegisterFields>
-                            <Stack spacing={5}>
-                                <C.DivInput>
-                                    <C.TextInput>CPF/CNPJ</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='email'
-                                        placeholder='Endereço de Email'
-                                    />
-                                </C.DivInput>
+                    </C.ContainerRegisterFields>
+                    <C.ContainerRegisterFields>
+                        <Stack spacing={5}>
+                            <C.DivInput>
+                                <C.TextInput>CPF/CNPJ</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    type={'number'}
+                                    value={cpfCnpj}
+                                    onChange={e => setCpfCnpj(e.target.value)}
+                                />
+                            </C.DivInput>
 
-                                <C.DivInput>
-                                    <C.TextInput>Nome Fantasia</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='password'
-                                        placeholder='Senha'
-                                        type={'password'}
-                                    />
-                                </C.DivInput>
-                                <C.DivInput>
-                                    <C.TextInput>Telefone</C.TextInput>
-                                    <Input
-                                        width={400}
-                                        height={50}
-                                        name='password'
-                                        placeholder='Senha'
-                                        type={'password'}
-                                    />
-                                </C.DivInput>
-                                <C.ContainerButtonRegister>
-                                    <Button width={'300px'} bg="primary">CADASTRAR</Button>
-                                </C.ContainerButtonRegister>
-                            </Stack>
-                        </C.ContainerRegisterFields>
-                    </C.ContainerRow>
-                </C.ContainerColumn>
+                            <C.DivInput>
+                                <C.TextInput>Nome Fantasia</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    value={nomeFantasia}
+                                    onChange={e => setNomeFantasia(e.target.value)}
+                                />
+                            </C.DivInput>
+                            <C.DivInput>
+                                <C.TextInput>Telefone</C.TextInput>
+                                <Input
+                                    width={400}
+                                    height={50}
+                                    type={'number'}
+                                    value={phone}
+                                    onChange={e => setPhone(e.target.value)}
+                                />
+                            </C.DivInput>
+                            <C.ContainerButtonRegister>
+                                <Button onClick={createUser} width={'300px'} bg="primary">CADASTRAR</Button>
+                            </C.ContainerButtonRegister>
+                        </Stack>
+                    </C.ContainerRegisterFields>
+                </C.ContainerRow>
+            </C.ContainerColumn>
         </React.Fragment>
     );
 }
