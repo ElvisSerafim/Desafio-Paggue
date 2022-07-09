7b8073f9b45a85eb61375409790bd9f283b7a5e4
ElvisSerafim <elvisdouza@outlook.com>
2022-07-08 22:54:24 -0300

    Change Header when a user is logged.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,15 @@
-import React, {useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import * as C from '../../styles/styles';
 import Profile from '../../assets/profile.png';
 import Logo from '../../assets/logo.png';
-import {FaBars} from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import Sidebar from '../SideBar';
+import { Link } from 'react-router-dom';
 
 import AuthContext from '../../contexts/auth';
 
 
-export default function Header(props){
+export default function Header(props) {
     const { signed } = useContext(AuthContext);
     const [showSideBar, setShowSideBar] = useState(false);
 
@@ -19,10 +20,18 @@ export default function Header(props){
             <C.ContainerHeader>
                 {signed === true ? (
                     <FaBars size={25} cursor={'pointer'} onClick={openSideBar} />
-                ): <></>}
-                {showSideBar && <Sidebar active={openSideBar} />} 
+                ) : <></>}
+                {showSideBar && <Sidebar active={openSideBar} />}
                 <C.Icon height={'100px'} width={'170px'} src={Logo} />
-                <C.Icon height={'35px'} width={'35px'} src={Profile}/>
+                {signed === true ? (
+                    <Link to={'/profile'}>
+                        <C.Icon height={'35px'} width={'35px'} src={Profile} />
+                    </Link>
+                ) :
+                    <Link to={'/'}>
+                        <C.Icon height={'35px'} width={'35px'} src={Profile} />
+                    </Link>
+                }
             </C.ContainerHeader>
         </React.Fragment>
     )
